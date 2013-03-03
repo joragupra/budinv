397985df90943a0f14ca1c5f96c35911ed16d5a9
Jorge Agudo Praena <joragupra@gmail.com>
2013-03-03 13:35:15 +0100

    Compilation problems fixed.

--- budinv-model/src/main/java/com/joragupra/budinv/model/Expense.java
+++ budinv-model/src/main/java/com/joragupra/budinv/model/Expense.java
@@ -1,10 +1,18 @@
 package com.joragupra.budinv.model;
 
+import java.util.Date;
+
 /**
  * @author: jagudo
  * @since: 3/03/13
  */
 public class Expense extends BookkeepingEntry {
 
-    private Category category;
+    public Expense(Date date, Double amount, String title) {
+        super(date, amount, title);
+    }
+
+    public Expense(Date date, Double amount, String title, Category category) {
+        super(date, amount, title, category);
+    }
 }
--- budinv-model/src/main/java/com/joragupra/budinv/model/Income.java
+++ budinv-model/src/main/java/com/joragupra/budinv/model/Income.java
@@ -1,8 +1,18 @@
 package com.joragupra.budinv.model;
 
+import java.util.Date;
+
 /**
  * @author: jagudo
  * @since: 3/03/13
  */
 public class Income extends BookkeepingEntry {
+
+    public Income(Date date, Double amount, String title) {
+        super(date, amount, title);
+    }
+
+    public Income(Date date, Double amount, String title, Category category) {
+        super(date, amount, title, category);
+    }
 }
