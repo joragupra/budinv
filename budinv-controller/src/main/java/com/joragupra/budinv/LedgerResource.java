1cb406280808f9f05a3f7fabed5171ea54b2c2a8
Jorge Agudo Praena <joragupra@gmail.com>
2024-02-13 12:14:59 +0100

    Change to RestController annotation in REST endpoint implementation

--- budinv-controller/src/main/java/com/joragupra/budinv/LedgerResource.java
+++ budinv-controller/src/main/java/com/joragupra/budinv/LedgerResource.java
@@ -7,10 +7,9 @@ import com.joragupra.budinv.dto.Ledger;
 import com.joragupra.budinv.dto.LedgerTransformer;
 import com.joragupra.budinv.repository.LedgerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
@@ -18,7 +17,7 @@ import java.util.Date;
  * Hello world!
  * 
  */
-@Controller
+@RestController
 @RequestMapping(value = "/ledger")
 public class LedgerResource {
 
@@ -26,13 +25,11 @@ public class LedgerResource {
     private LedgerRepository repo;
 
     @RequestMapping(value = "hello", method = RequestMethod.GET)
-    @ResponseBody
     public String hello() {
         return "Hello world";
     }
 
     @RequestMapping(value = "/show/list", method = RequestMethod.GET)
-    @ResponseBody
 	public Ledger getLedger() {
 
         repo.findAll();
